a2443994990622b9f9bd1d6e5ff5ca2d98bbebd4
MartinaP87 <bleach1987@hotmail.it>
2022-12-30 17:20:52 +0000

    first react challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import css from './App.module.css';
+import ItWorks from './components/Sidebar';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
+      <ItWorks/>
     </div>
   );
 }
