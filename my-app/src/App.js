333a38415a430afacd0d8101c3624465857605bd
MartinaP87 <bleach1987@hotmail.it>
2023-01-04 14:40:06 +0000

    Rendering list challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,13 @@ import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
+import Content from './components/Content';
 
 
 function App() {
   return (
     <div className={css.App}>
-      <NavBarForm/>
+      <Content/>
     </div>
   );
 }
